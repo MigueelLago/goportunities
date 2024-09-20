e7ccc16930b83e343e560e713af05d12a5191382
miguellago <miguelbaned.23@gmail.com>
2024-09-20 11:43:20 -0300

    feat: add logger customizado

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"github.com/MigueelLago/goportunities/config"
 	"github.com/MigueelLago/goportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	// Inicializa as configs
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("Erro na inicialização: %v", err)
+		return
+	}
 
 	// Initialize router
 	router.Initialize()
