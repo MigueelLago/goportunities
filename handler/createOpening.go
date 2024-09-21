54600da8fb48cfccdb0d5730437895a71cfdecb3
miguellago <miguelbaned.23@gmail.com>
2024-09-20 22:03:45 -0300

    feat: handler store criado

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,36 @@ package handler
 import (
 	"net/http"
 
+	"github.com/MigueelLago/goportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.ErrorF("erro de validação: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.ErrorF("erro ao criar dados no banco de dados: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "erro ao criar tabela no banco")
+		return
+	}
+
+	sendSuccess(ctx, "Vaga registrada!", opening)
 }
